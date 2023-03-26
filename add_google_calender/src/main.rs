8274f037aabfd73fc0990698190f7daf27795edb
HAYASHI TAKAO <rem@macbook-3.local>
2023-03-27 08:01:01 +0900

    WIP fix error

--- add_google_calender/src/main.rs
+++ add_google_calender/src/main.rs
@@ -1,39 +1,33 @@
-use google_calendar::{Calendar, Event, InsertableEvent};
-use google_authz::Authenticator;
-use google_oauth2::{AccessToken, UserCredentials};
-use std::env;
+use google_calendar3::api::{Event, Events, Error};
+use yup_oauth2::{read_service_account_key, ServiceAccountAuthenticator};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client_id = env::var("CLIENT_ID")?;
-    let client_secret = env::var("CLIENT_SECRET")?;
-    let refresh_token = env::var("REFRESH_TOKEN")?;
+fn main() -> Result<()> {
+    let sa_key = read_service_account_key("path/to/key.json").unwrap();
+    let auth = ServiceAccountAuthenticator::builder(sa_key)
+        .build()
+        .unwrap();
 
-    let auth = Authenticator::new(
-        &client_id,
-        &client_secret,
-        UserCredentials::new(
-            None,
-            None,
-            Some(refresh_token),
-            None,
-        ),
-        None,
+    // カレンダーAPIクライアントを初期化する
+    let calendar = google_calendar3::Calendar::new(
+        hyper::Client::builder().build_http(),
+        auth,
+        Default::default(),
     );
 
-    let access_token = auth.access_token()?;
-    let calendar = Calendar::new(AccessToken::new(access_token.secret().to_string()));
+    let today = chrono::Local::today().format("%Y-%m-%d").to_string();
 
-    let event = InsertableEvent {
-        summary: "Test Event".to_string(),
-        description: Some("This is a test event".to_string()),
-        start_date_time: "2023-02-01T09:00:00-07:00".to_string(),
-        end_date_time: "2023-02-01T10:00:00-07:00".to_string(),
-        ..InsertableEvent::default()
-    };
+    let events: Events = calendar
+        .events()
+        .list("primary")
+        .time_min(&today)
+        .time_max(&today)
+        .doit()
+        .map_err(Error::from)?;
 
-    let inserted_event = calendar.insert_event("primary", &event)?;
-
-    println!("Event created: {}", inserted_event.html_link);
+    // イベントを出力する
+    for event in events.items.unwrap() {
+        println!("{}", event.summary.unwrap());
+    }
 
     Ok(())
 }
